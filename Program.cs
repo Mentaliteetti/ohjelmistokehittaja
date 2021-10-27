d63a516888f9e970b20f93881b568b08ecb30f49
Mentaliteetti <62391429+Mentaliteetti@users.noreply.github.com>
2021-10-27 11:33:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 12, b = 11;
+            if (a > b) ;
+            Console.WriteLine()
+                              
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+            
+        
+        
+
+
+
+    
+    
+
