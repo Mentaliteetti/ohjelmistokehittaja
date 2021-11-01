87bc495f8fbde27111352a4339a2b980382a0001
Mentaliteetti <62391429+Mentaliteetti@users.noreply.github.com>
2021-11-01 09:13:49 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace laskuharjoituksia
 {
     class Program
     {
-        static void Main(string[] args)
+         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int a = 12, b = 11;
-            if (a > b) ;
-            Console.WriteLine()
-                              
-
-
-
-
-
-
+            //Harjoitus 1
+            Console.Write("Anna ensimmäinen numero");
+            int Luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna Toinen numero");
+            int Luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Luku1 = Luku2 + 3);
+            {
+                //Harjoitus 2
+                Console.Write("Anna ensimmäinen numero");
+                int Luku1 = int.Parse(Console.ReadLine());
+                Console.Write("Anna Toinen numero");
+                int Luku2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(Luku1 = Luku2 + 3);
+            }
+            {
+                //Harjoitus 3
+                Console.Write("Anna ensimmäinen numero");
+                int Luku1 = int.Parse(Console.ReadLine());
+                Console.Write("Anna Toinen numero");
+                int Luku2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(Luku1 = Luku2 + 3);
+            }
+       
         }
-
     }
 }
-
-
-            
-        
-        
-
-
-
-    
-    
-
